be9d77e55b0653b0f903a518bb52266b1086d4af
dmkirshon <kirshon.david@gmail.com>
2022-05-16 14:21:41 +0200

    feat: Add random card placement

--- src/components/gameboard/Card.js
+++ src/components/gameboard/Card.js
@@ -1,9 +1,16 @@
 import React from "react";
+import emojis from "../assets/emojis.json";
 
-const Card = () => {
+const Card = ({ emojiIndex }) => {
+  function insertEmoji(index) {
+    return Object.entries(emojis)[index][1];
+  }
+
   return (
     <div className="game-main-gameboard-card">
-      <div className="game-main-gameboard-card-emoji">🙂</div>
+      <div className="game-main-gameboard-card-emoji">
+        {insertEmoji(emojiIndex)}
+      </div>
     </div>
   );
 };
--- src/components/gameboard/GameBoard.js
+++ src/components/gameboard/GameBoard.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 
 const GameBoard = () => {
-  return (
-    <div className="game-main-gameboard">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
+  function placeCards() {
+    const randomCardDigits = [];
+    for (let i = 0; i < 12; i++) {
+      while (randomCardDigits.length < i + 1) {
+        const randomDigit = Math.floor(Math.random() * 12);
+        if (!randomCardDigits.includes(randomDigit))
+          randomCardDigits[i] = randomDigit;
+      }
+    }
+    const cardPlacement = randomCardDigits.map((cardNumber) => (
+      <Card key={cardNumber} emojiIndex={cardNumber} />
+    ));
+    return cardPlacement;
+  }
+
+  return <div className="game-main-gameboard">{placeCards()}</div>;
 };
 
 export default GameBoard;
