542397fcd2c54b5a7287ddd63be9aadecf68e763
dmkirshon <kirshon.david@gmail.com>
2022-05-17 10:28:52 +0200

    feat: Add states for game structure

--- src/components/gameboard/GameBoard.js
+++ src/components/gameboard/GameBoard.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 const GameBoard = () => {
+  const NUMBER_OF_CARDS = 12;
+
+  const [selectedEmojis, setSelectedEmojis] = useState([]);
+  const [placedEmojis, setplacedEmojis] = useState(placeCards());
+
+  function checkWinGame() {
+    if (selectedEmojis.length === NUMBER_OF_CARDS) {
+      console.log("You Win");
+    }
+  }
+
   function placeCards() {
     const randomCardDigits = [];
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < NUMBER_OF_CARDS; i++) {
       while (randomCardDigits.length < i + 1) {
-        const randomDigit = Math.floor(Math.random() * 12);
+        const randomDigit = Math.floor(Math.random() * NUMBER_OF_CARDS);
         if (!randomCardDigits.includes(randomDigit))
           randomCardDigits[i] = randomDigit;
       }
@@ -17,7 +28,7 @@ const GameBoard = () => {
     return cardPlacement;
   }
 
-  return <div className="game-main-gameboard">{placeCards()}</div>;
+  return <div className="game-main-gameboard">{placedEmojis}</div>;
 };
 
 export default GameBoard;
